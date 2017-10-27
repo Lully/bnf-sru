6c45844f4ebdf12b7366956d21aa583563341ef2
Lully <lully1804@yahoo.fr>
2017-10-27 21:40:46 +0200

    Fichier setup d'ExtractionCatalogue
    Màj avec variables locales

--- setup_ExtractionCatalogueBnF.py
+++ setup_ExtractionCatalogueBnF.py
@@ -1,16 +1,16 @@
 """Fichier d'installation du script ExtractionCatalogueBnF_code.py."""
-#Commande Windows à utiliser : C:\python\Anaconda\python.exe setup.py build
+#Commande Windows à utiliser : C:\ProgramData\Anaconda3\python.exe setup_ExtractionCatalogueBnF.py build
 
 from cx_Freeze import setup, Executable
 import os
 
-os.environ['TCL_LIBRARY'] = "C:\\python\\Anaconda\\tcl\\tcl8.6"
-os.environ['TK_LIBRARY'] = "C:\\python\\Anaconda\\tcl\\tk8.6"
+os.environ['TCL_LIBRARY'] = "C:\\ProgramData\\Anaconda3\\tcl\\tcl8.6"
+os.environ['TK_LIBRARY'] = "C:\\ProgramData\\Anaconda3\\tcl\\tk8.6"
 # On appelle la fonction setup
 
 includes  = []
-include_files = ["C:\\python\\Anaconda\\DLLs\\tcl86t.dll",
-                 "C:\\python\\Anaconda\\DLLs\\tk86t.dll"]
+include_files = ["C:\\ProgramData\\Anaconda3\\DLLs\\tcl86t.dll",
+                 "C:\\ProgramData\\Anaconda3\\DLLs\\tk86t.dll"]
 base = None
 
 build_exe_options = {"packages": ["files", "tools"], "include_files": ["tcl86t.dll", "tk86t.dll"]}  
