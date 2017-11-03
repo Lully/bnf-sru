3a4ec7ce1ab02b5e1abcdc7d88390ea1e0bf66f0
Lully <lully1804@yahoo.fr>
2017-11-03 06:45:08 +0100

    Référencement des dernières compilations
    Fichier qui sera interrogé par les exécutables pour vérifier si une version plus récente est disponible -> proposera un lien de téléchargement

---
+++ last_compilations.py
@@ -0,0 +1,5 @@
+"""Fichier fournissant le numéro de la dernière version compilée pour plusieurs programmes liés au SRU
+Lorsqu'on lance un de ces programmes, il vérifie sa propre version et la compare avec celle de ce fichier.
+Si celle du fichier est supérieure, il propose de la télécharger"""
+
+last_compilations = {"ExtractionCatalogueBnF": 0.3}
