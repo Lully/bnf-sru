cae3083a01a2b5c2642aead9751e76d7258b5121
Lully <lully1804@yahoo.fr>
2017-11-12 09:05:38 +0100

    Compilation ExtractionCatalogueBnF

--- last_compilations.py
+++ last_compilations.py
@@ -1,4 +1,4 @@
-{"last_compilations": [{"ExtractionCatalogueBnF": 0.5,
+{"last_compilations": [{"ExtractionCatalogueBnF": 0.6,
                         "ExtractionWebCCA":11,
                        "noticesbib2arkBnF":0.6,
                        "transbiblio":0.03}]}
