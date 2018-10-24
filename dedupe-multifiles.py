de57164d27a0a95130873d05ba11e8896388bdb9
Lully <lully1804@yahoo.fr>
2018-10-24 11:30:44 +0200

    Màj dedupe-multifile
    Option pour récupérer toutes les métadonnées associées à un identifiant dans un fichier

--- dedupe-multifiles.py
+++ dedupe-multifiles.py
@@ -58,12 +58,12 @@ def select_column(select_input, headers):
         return headers.index(select_input)
 
 
-def file2dic(filename, select_col):
+def file2dic(filename, select_col, recup_meta):
     if ("xls" in filename):
         # ouverture du fichier Excel
-        excel_file2dict(filename, select_col)
+        excel_file2dict(filename, select_col, recup_meta)
     else:
-        csv_file2dict(filename, select_col)
+        csv_file2dict(filename, select_col, recup_meta)
 
 
 def select_column_xls(select_input, headers):
@@ -78,11 +78,10 @@ def select_column_xls(select_input, headers):
             i += 1
     # Conversion de l'indice de colonne en lettre de colonne
     col = string.ascii_uppercase[col]
-    print(col)
     return col
 
 
-def excel_file2dict(input_filename, select_col):
+def excel_file2dict(input_filename, select_col, recup_meta):
     xlsfile = load_workbook(filename=input_filename)
     xls_table_name = xlsfile.sheetnames[0]
     xls_table = xlsfile[xls_table_name]
@@ -92,10 +91,15 @@ def excel_file2dict(input_filename, select_col):
         identifier = xls_table[cell_name].value
         if (identifier):
             dic_ids[identifier].add(filename)
-            dic_id2metas[identifier+input_filename] = row
+        if (recup_meta == "o"):
+            key = identifier + input_filename
+            for col in range(0, xls_table.max_column):
+                cell = string.ascii_uppercase[col]+str(row)
+                dic_id2metas[key].append(xls_table[cell].value)
 
 
-def csv_file2dict(input_filename, select_col):
+
+def csv_file2dict(input_filename, select_col, recup_meta):
     with open(input_filename, encoding="utf-8") as csvfile:
             content  = csv.reader(csvfile, delimiter='\t')
             headers = next(content)
@@ -103,21 +107,25 @@ def csv_file2dict(input_filename, select_col):
             for row in content:
                 identifier = row[column_id]
                 dic_ids[identifier].add(input_filename)
-                dic_id2metas[identifier+input_filename] = row
+                if (recup_meta == "o"):
+                    dic_id2metas[identifier+input_filename] = row
 
 
     
 
-def create_csv_report(output_filename, output_filetype):
+def create_csv_report(output_filename, output_filetype, recup_meta):
     report = open(output_filename, "w", encoding="utf-8")
     report.write("\t".join["Identifiant doublon", "fichiers concernés"] + "\n")
     return report
 
 
-def create_xls_report(output_filename, output_filetype):
+def create_xls_report(output_filename, output_filetype, recup_meta):
     report = Workbook(write_only=False)
     report.guess_type = False
     report_table = report.create_sheet(title="liste_doublons")
+    if ("Sheet" in report.get_sheet_names()):
+        sh1 = report.get_sheet_by_name("Sheet")
+        report.remove_sheet(sh1)
     report_table["A1"] = "Identifiant doublon"
     report_table["B1"] =  "fichiers concernés"
     report.save(os.path.join(dir_path, output_filename))
@@ -129,42 +137,52 @@ def line2report(line, report, output_filetype, i):
     if (output_filetype == "csv"):
         report.write("\t".join(line) + "\n")
     else:
-        cellA = "A" + str(i)
-        cellB = "B" + str(i)
-        report[cellA] = str(line[0])
-        report[cellB] = str(line[1])
+        j = 0
+        for el in line:
+            cell_adress = string.ascii_uppercase[j]+str(i)
+            report[cell_adress] = str(line[j])
+            j += 1
 
 
-def dict_entry2report(report, entry, entry_value, i):
-    line = [entry, ",".join(list(dic_ids[entry]))]
-    print(line)
-    line2report(line, report, output_filetype, i)
+def dict_entry2report(report, entry, entry_value, i, output_filetype, recup_meta):
+    for source in list(dic_ids[entry]):
+        i += 1
+        line = [entry, source]
+        if (recup_meta == "o"):
+            key = entry + source
+            meta = dic_id2metas[key]
+            line.extend(meta)
+        print(line)
+        line2report(line, report, output_filetype, i)
+    return i
     
-
 
 if __name__ =="__main__":
     filelist = input("Nom des fichiers à comparer (séparés par des ';') : ")
     select_col = input("Nom ou numéro de colonne (numérotation commençant à 1) \
 servant d'identifiant (par défaut : 1ère colonne) : ")
+    recup_meta = input("Récupérer toutes les métadonnées (O/N) ? ").lower()
+    if (recup_meta == ""):
+        recup_meta = "o"
     output_filename = input("Nom du rapport de doublons : ")
     output_filetype = "csv"
     for filename in filelist.split(";"):
-        file2dic(filename, select_col)
+        file2dic(filename, select_col, recup_meta)
 
     if (".xls" in output_filename):
         output_filetype = "xlsx"
-        report, sheet = create_xls_report(output_filename, output_filetype)
+        report, sheet = create_xls_report(output_filename, output_filetype, recup_meta)
     else:
-        report = create_csv_report(output_filename, output_filetype)
+        report = create_csv_report(output_filename, output_filetype, recup_meta)
     
     i = 2
     if (".xls" in output_filename):
         for entry in dic_ids:
             if len(dic_ids[entry]) > 1:
-                dict_entry2report(sheet, entry, dic_ids[entry], i)
-                i += 1
+                i = dict_entry2report(sheet, entry, dic_ids[entry], i, output_filetype, recup_meta)
+                
         report.save(os.path.join(dir_path, output_filename))
     else:
         for entry in dic_ids:
             if len(dic_ids[entry]) > 1:
-                dict_entry2report(report, entry, dic_ids[entry], i)
+                i = dict_entry2report(report, entry, dic_ids[entry], i, output_filetype, recup_meta)
