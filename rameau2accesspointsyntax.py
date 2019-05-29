815be0f84807f4915d935bcadfef206beafdf4d6
Lully <lully1804@yahoo.fr>
2019-05-29 11:39:33 +0200

    extraction template points d'accès Rameau

--- rameau2accesspointsyntax.py
+++ rameau2accesspointsyntax.py
@@ -2,6 +2,29 @@
 
 """
 A partir des notices Rameau existantes, extraire la syntaxe
+
+Résultat sur quelques milliers de notices :
+'a b f x': {'\\1, \\2 (\\3) -- \\4'}, 
+'a y': {'\\1 -- \\2},
+'a y x': {'\\1 -- \\2 -- \\3'},
+'a y y': {'\\1 -- \\2 -- \\3'},
+'a b x': {'\\1. \\2 -- \\3'},
+'a x x': {'\\1 -- \\2 -- \\3'},
+'a c b x': {'\\1. \\3 (\\2) -- \\4',
+'a y z': {'\\1 -- \\2 -- \\3'},
+'a c': {'\\1 (\\2)'},
+'a c f x': {'\\1 (\\2 ; \\3) -- \\4'},
+'a b c x': {'\\1. \\2 (\\3) -- \\4'},
+'a x y': {'\\1 -- \\2 -- \\3'},
+'a b c': {'\\1, \\2 (\\3)'},
+'a y y z': {'\\1 -- \\2 -- \\3 -- \\4'},
+'a z x': {'\\1 -- \\3 -- \\2'},
+'a b x x': {'\\1. \\2 -- \\3 -- \\4'},
+'a c x x': {'\\1 (\\2) -- \\3 -- \\4'},
+'a b d x': {'\\1, \\2 \\3 -- \\4'},
+'a f x': {'\\1 (\\2) -- \\3'},
+'a b c b x': {'\\1. \\2 (\\3) -- \\5'}})
+
 """
 
 from collections import defaultdict
@@ -23,10 +46,12 @@ def query2extract(query, report):
     while i < result.nb_results:
         param_default = {"startRecord": str(i)}
         next_result = sru.SRU_result(query, parametres = param_default)
+        print(next_result.url)
         for ark in next_result.dict_records:
             analyse_record(ark, next_result.dict_records[ark], dict_rules)
         i += 1000
-
+        print(dict_rules)
+    eot(report, dict_rules)
 
 def analyse_record(ark, xml_record, dict_rules):
     # Analyse d'une notice d'autorité Rameau (Unimarc)
@@ -50,7 +75,8 @@ def extract_accesspoint(ark, datafield):
     for subfield in datafield.xpath("*[@code]"):
         code = subfield.get("code")
         value = subfield.text
-        field_value.append([f"${code} {value}"])
+        if code in string.ascii_lowercase:
+            field_value.append(f"${code} {value}")
     field_value = " ".join(field_value)
     ark_id = ark.split("/")[-1]
     url_rdf = f"https://data.bnf.fr/sparql?default-graph-uri=&\
@@ -63,11 +89,12 @@ UNION%0D%0A++\
 %7B%3Chttp%3A%2F%2Fdata.bnf.fr%2Fark%3A%2F12148%2F{ark_id}%3E+skos%3AprefLabel+%3Flabel%7D%0D%0A%7D\
 &format=application%2Frdf%2Bxml&timeout=0&should-sponge=&debug=on"
     test, result = testURLetreeParse(url_rdf)
-    ns = {"skos":"http://www.w3.org/2004/02/skos/core#"}
+    ns = {"skos":"http://www.w3.org/2004/02/skos/core#",
+          "res": "http://www.w3.org/2005/sparql-results#"}
     label = ""
     if (test
-        and result.find("//skos:prefLabel", namespaces=ns) is not None):
-        label = result.find("//skos:prefLabel", namespaces=ns).text
+        and result.find("//res:value", namespaces=ns) is not None):
+        label = result.find("//res:value", namespaces=ns).text
     return label, field_value, liste_subfields
 
 
@@ -77,21 +104,23 @@ def extract_rules(accesspoint, field_value, liste_subfields, dict_rules):
     zone Unimarc, on extrait des règles
     """
     field_value = [el.strip() for el in field_value.split("$") if el.strip()]
-    pos = 0
     i = 1
-    rule = ""
+    rule = accesspoint
     for subfield in field_value:
         subf_val = subfield[2:]
-        rule += accesspoint[pos:].replace(subf_val, f"\\{i}")
-        print(f"chercher '{subf_val}' dans '{accesspoint[pos:]}'", f"i = {i}")
-        pos += len(subf_val)
+        rule = rule.replace(subf_val, f"\\{i}", 1)
         i += 1
-        print("rule", rule)
+    dict_rules[liste_subfields].add(rule)
      
-    
-
+def eot(report, dict_rules):
+    for entry in dict_rules:
+        values = list(dict_rules[entry])
+        for value in values:
+            print(entry, value)
+            line2report([entry, value], report)
+    close_files([report])
 
 if __name__ == "__main__":
-    query = input("Requête SRU (Unimarc) :")
+    query = input("Requête SRU (Unimarc) : ")
     report = create_file("syntaxe-accesspoint-rameau.txt")
     query2extract(query, report)
