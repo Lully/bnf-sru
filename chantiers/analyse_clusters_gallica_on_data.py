35b90379e94fb542e1de7bc9bc242ec628f1553c
Lully <lully1804@yahoo.fr>
2019-11-22 09:26:09 +0100

    Gallica on Data

--- chantiers/analyse_clusters_gallica_on_data.py
+++ chantiers/analyse_clusters_gallica_on_data.py
@@ -19,11 +19,12 @@ from cleanstring import clean_string
 from stdf import *
 
 class Work:
-    def __init__(self, cluster):
-        self.arkAUT = row[1]
+    def __init__(self, clusterid, dict_cluster):
+        row_ref = dict_cluster["row"]["0"]
+        self.arkAUT = row_ref[2]
         self.recordAUT = ark2autrecord(arkAUT)
         self.names = autrec2accesspoint(self.recordAUT)
-        self.title = String(row[header.find("Titre Oeuvre")])
+        self.title = String(row_ref[header.find("Titre de l'oeuvre")])
         
 
 class String:
@@ -37,7 +38,7 @@ def autrec2accesspoint(aut_record):
     names = [String(name) for name in names]
     return names
 
-def controles_titres(record):
+def controles_titres(Work):
     """
     Ensembles de contrôles qui permettent d'identifier des cas où les 
     oeuvres générées sont suspectes
@@ -56,9 +57,70 @@ def controles_titres(record):
     
 
 
-def analyse_files(clustersTIC_filename, clustersMON_sans_TIC_filename, clustersAGR_sans_TIC_filename, report):
-    dict_works_keys = defaultdict(dict)
+def analyse_files(dict_files, report):
+    dict_keys2works = defaultdict(set)
+    dict_clustersid2content = defaultdict(dict)
+    for filename in dict_files:
+        with open(dict_files[filename], encoding="utf-8") as file:
+            content = csv.reader(file, delimiter="\t")
+            headers = next(content)
+            for row in content:
+                clusterid = row[headers.find("clusterid")]
+                ark_manif = row[headers.find("ark manifestation")]
+                work_title = row[headers.find("Titre de l'oeuvre")]
+                if clusterid in dict_clustersid2content:
+                    dict_clustersid2content[clusterid]["row"].append(row)
+                    dict_clustersid2content[clusterid]["key"].add(ark_manif)
+                    dict_clustersid2content[clusterid]["arks_manifs"].add(ark_manif)
+                    dict_clustersid2content[clusterid]["key"].add(work_title)
+                    dict_clustersid2content[clusterid]["tic"].add(row2tic(row, headers))
+                else:
+                    dict_clustersid2content[clusterid] = {"row": [row],
+                                                           "filename": filename,
+                                                           "arks_manifs": {ark_manif},
+                                                           "tic" = {row2tic(row, headers)}
+                                                           "key": {ark_manif, work_title}}
+    for clusterid in dict_clustersid2content:
+        hashid = sorted(list(dict_clustersid2content[clusterid]["key"]))
+        hashid = hash(hashid)
+        dict_clustersid2content[clusterid]["hash"] = hashid
+        tic = [el for el in list(dict_clustersid2content[clusterid]["tic"]) if el]
+        tic = ",".join(tic)
+        dict_clustersid2content[clusterid]["tic"] = tic
+    for clusterid in dict_clustersid2content:
+        dict_keys2works[dict_clustersid2content[clusterid]["hash"]].add(clusterid)
+    analyse_dicts(dict_keys2works, dict_clustersid2content, dict_files, report)
 
+
+def row2tic(row, headers):
+    if "ark Oeuvre" in headers and row[headers.find("ark Oeuvre")]:
+        return row[headers.find("ark Oeuvre")]
+    else:
+        return ""
+
+
+def analyse_dicts(dict_keys2works, dict_clustersid2content, dict_files, report):
+    for work_key in dict_keys2works:
+        multi_aut = False
+        test_pexnum = False
+        test_lienmanif_pexnum = False
+        test_tic = False
+        tic = ""
+        if count(list(dict_keys2works[work_key])) > 1:
+            multi_aut = True
+        for clusterid in dict_keys2works[work_key]:
+            work = Work(clusterid, dict_clustersid2content[clusterid])
+            if dict_clustersid2content[clusterid]["tic"]:
+                test_tic = True
+                tic = dict_clustersid2content[clusterid]["tic"]
+            controles = controles_titres(work)
+            test_pexnum = check_pexnum(dict_clustersid2content[clusterid]["arks_manifs"])
+
+            
+
+def check_pexnum(arks_manifs):
+    test = False
+    
 
 
 
@@ -68,4 +130,7 @@ if __name__ == "__main__":
     clustersAGR_sans_TIC_filename = "agregats_sansTIC.txt"
     report = create_file("Liste_tous_clusters.txt",
                          "ARK AUT,Nom AUT,ClusterID,TitreOeuvre,TIC ?,PEX NUM ?,Manif avec PEX NUM liée à TIC?".split(","))
-    analyse_files(clustersTIC_filename, clustersMON_sans_TIC_filename, clustersAGR_sans_TIC_filename, report)
+    dict_files = {"clustersTIC": clustersTIC_filename,
+                  "clustersMONsansTIC": clustersMON_sans_TIC_filename,
+                  "clustersAGRsansTIC": clustersAGR_sans_TIC_filename}
+    analyse_files(dict_files, report)
--- times.py
+++ times.py
@@ -5,24 +5,44 @@ import datetime
 
 from stdf import *
 
-def rewrite_times(inputfilename, decalage, outputfile):
+def rewrite_times(inputfilename, decalage, heure_debut_fin, outputfile):
     file = open(inputfilename, encoding="ansi")
     for line in file:
         line = line.replace("\n", "").replace("\r", "")
-        if re.fullmatch(r"[:\d]+.+ --> [:\d]+.+", line) is not None:
-            rewrite_line(line, decalage, outputfile)
+        if re.fullmatch(r"[:\d]+.+ --> ?[:\d]+.+", line) is not None:
+            rewrite_line(line, decalage, heure_debut_fin, outputfile)
         else:
             outputfile.write(line + "\n")
 
-def rewrite_line(line, decalage, outputfile):
+def rewrite_line(line, decalage, heure_debut_fin, outputfile):
     time1_init = line[0:8]
+    if (heure_debut_fin):
+        decalage = recalc_decalage(heure_debut_fin, time1_init)
+        print(decalage)
     time1 = convert_time(time1_init, decalage)
     time2_init = line[17:25]
+    if (heure_debut_fin):
+        decalage = recalc_decalage(heure_debut_fin, time2_init)
     time2 = convert_time(time2_init, decalage)
     line = time1 + line[8:16] + time2 + line[25:]
     # line[17:25] = time2
     # line = line.replace(time1_init, time1).replace(time2_init, time2)
     outputfile.write(line + "\n")
+
+def recalc_decalage(heure_debut_fin, time1_init):
+    # heure_debut_fin : 
+    # ["00:01:51","00:02:27", "01:47:24", "01:54:28"]
+    # converti en
+    # [111, 157, 3824, 3927]
+    heure_debut, nouv_heure_debut, heure_fin, nouv_heure_fin = heure_debut_fin
+    time1_sec = convert_heure_in_sec(time1_init)
+    ecart_debut = nouv_heure_debut-heure_debut
+    ecart_fin = nouv_heure_fin-heure_fin
+    decalage = (time1_sec-heure_debut)*ecart_fin/(heure_fin-heure_debut)
+    decalage = round(decalage, 0)
+    print(decalage)
+    return decalage
+    
 
 
 def convert_time(time_init, decalage):
@@ -39,8 +59,30 @@ def convert_time(time_init, decalage):
     return new_time
 
 
+def convert_heure_in_sec(hour):
+    hour = [int(el) for el in hour.split(":")]
+    seconds = hour[0]*3600 + hour[1]*60 + hour[2]
+    return seconds
+
 if __name__ == "__main__":
+    text = """Programme d'ajustement dans la synchronisation des sous-titres
+Le plus simple : il y a quelques secondes de décalage, même décalage sur toute la durée du film
+--> on renseigne la zone "Decalage"
+Une fois cela fait, si le décalage s'accroît au fil du film, il faut ajouter
+les équivalences de début et de fin :
+à quelle heure doit apparaître le premier sous-titre, et à quelle heure le dernier
+
+"""
+    print(text)
     inputfilename = input("Nom du fichier de sous-titres : ")
-    decalage = int(input("Decalage (secondes) : "))
+    decalage = input("Decalage (secondes) : ")
+    if decalage:
+        decalage = int(decalage)
+    heure_debut_fin = input("Ou heure_debut=Nouvelle_heure_debut/heure_fin=Nouvelle_heure_fin\n\
+Exemple : 00:01:51=00:02:27/01:47:24=01:54:28\n")
+    if heure_debut_fin:
+        heure_debut_fin = heure_debut_fin.split("/")[0].split("=") + heure_debut_fin.split("/")[1].split("=")
+        heure_debut_fin = [convert_heure_in_sec(el) for el in heure_debut_fin]
+
     outputfile = input2outputfile(inputfilename, "corrige.sub")
-    rewrite_times(inputfilename, decalage, outputfile)
+    rewrite_times(inputfilename, decalage, heure_debut_fin, outputfile)
