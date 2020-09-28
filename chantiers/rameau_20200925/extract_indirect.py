065266f832977c1690779f5613bde11d83d304e4
Lully <lully1804@yahoo.fr>
2020-09-28 09:41:59 +0200

    chantiers Rameau
    Extractions : indirect, sujets non autonomes

--- chantiers/rameau_20200925/extract_indirect.py
+++ chantiers/rameau_20200925/extract_indirect.py
@@ -7,33 +7,41 @@ explain = """Le groupe de suivi a grand besoin d'un fichier à trois onglets :
 
 from stdf import *
 import SRUextraction as sru
+from listes import depts_fr, us, arks167
 
-from listes import *
 
+depts_fr = [el[1] for el in depts_fr]
+us = [el[1] for el in us]
 
 def extract_concepts(tous_167, depts_fr_167, lieux_americains):
     for ark in arks167:
-        record = sru.SRU_result(f"idPerenne any \"{ark}\"", "http://noticesservices.bnf.fr/SRU", 
-                                {"recordSchema": "InterXmarc_Complet"}).firstRecord
+        #print(ark)
+        #record = sru.SRU_result(f"idPerenne any \"{ark}\"", "http://noticesservices.bnf.fr/SRU", 
+        #                        {"recordSchema": "InterXmarc_Complet"}).firstRecord
+        record = sru.SRU_result(f"aut.persistentid any \"{ark}\"", 
+                                parametres={"recordSchema": "intermarcxchange"}).firstRecord
         if record is not None:
             label = sru.record2fieldvalue(record, "167")
-            node167 = [node for node in record.xpath("*[@tag='167']")][0]
-            subfields = sru.field2listsubfields(node167)
-            f167g = sru.record2fieldvalue(record, "167$g")
-            others = []
-            for field in "460,461,462,463,464,465,466,467,468".split(","):
-                for occ_field in record.xpath(f"*[@tag='{field}']"):
-                    value = f"{field} {sru.field2value(field)}"
-                    others.append(value)
-            others = "¤".join(others)
-            line2report([ark, ark2nn(ark), label, subfields, others], tous_167)
-            if f167g in depts_fr_167:
-                line2report([ark, ark2nn(ark), label, f167g, subfields, others], depts_fr_167)
-            if f167g in lieux_americains:
-                line2report([ark, ark2nn(ark), label, f167g, subfields, others], lieux_americains)
+            try:
+                node167 = [node for node in record.xpath("*[@tag='167']")][0]
+                subfields = sru.field2listsubfields(node167)
+                f167g = sru.record2fieldvalue(record, "167$g")
+                others = []
+                for field in "460,461,462,463,464,465,466,467,468".split(","):
+                    for occ_field in record.xpath(f"*[@tag='{field}']"):
+                        value = f"{field} {sru.field2value(occ_field)}"
+                        others.append(value)
+                others = "¤".join(others)
+                line2report([ark, ark2nn(ark), label, subfields, others], tous_167)
+                if f167g in depts_fr:
+                    line2report([ark, ark2nn(ark), label, f167g, subfields, others], depts_fr_167)
+                if f167g in us:
+                    line2report([ark, ark2nn(ark), label, f167g, subfields, others], lieux_americains)
+            except IndexError:
+                pass
 
 if __name__ == "__main__":
-    tous_167 = create_file("tous_167.txt", "ARK,NNA,Label,listes sous-zones,Formes rejetées".split(","))
-    depts_fr_167 = create_file("depts_fr_167.txt", "ARK,NNA,Label,$g,listes sous-zones,Formes rejetées".split(","))
-    lieux_americains = create_file("lieux_americains.txt", "ARK,NNA,Label,$g,listes sous-zones,Formes rejetées".split(","))
+    tous_167 = create_file("tous_167-2.txt", "ARK,NNA,Label,listes sous-zones,Formes rejetées".split(","))
+    depts_fr_167 = create_file("depts_fr_167-2.txt", "ARK,NNA,Label,$g,listes sous-zones,Formes rejetées".split(","))
+    lieux_americains = create_file("lieux_americains-2.txt", "ARK,NNA,Label,$g,listes sous-zones,Formes rejetées".split(","))
     extract_concepts(tous_167, depts_fr_167, lieux_americains)
--- chantiers/rameau_20200925/subdiv_sujets_non_propres.py
+++ chantiers/rameau_20200925/subdiv_sujets_non_propres.py
@@ -19,7 +19,7 @@ def extract_rameau(report):
 def analyse_ark(ark, xml_record, subdivs, report):
     fields = [str(el) for el in range(160, 170)] + [str(el) for el in range(460, 470)]
     for field in fields:
-        for field_occ in xml_record.xpath(f".[@tag='{field}']"):
+        for field_occ in xml_record.xpath(f"*[@tag='{field}']"):
             field_value = sru.field2value(field_occ)
             for subf_x in field_occ.xpath("*[@code='x']"):
                 value = subf_x.text
@@ -29,33 +29,78 @@ def analyse_ark(ark, xml_record, subdivs, report):
     return subdivs
 
 
-def analyse_value(field, field_value, value, ark, xml_record, subdivs, report):
+def analyse_value_sparql(field, field_value, value, ark, xml_record, subdivs, report):
     if value in subdivs:
         if subdivs[value] == "non autonome":
-            line2report([ark, nn2ark(ark), field, field_value, value])
+            line2report([ark, nn2ark(ark), field, field_value, value], report)
     else:
         sparql_query = """PREFIX dcterms: <http://purl.org/dc/terms/>
 PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 select ?ark ?label where {
-  ?ark a skos:Concept; dcterms:isPartOf ?ensemble.
-  ?ark skos:prefLabel \"""" + value + """\"."""
+  ?ark a skos:Concept; dcterms:isPartOf ?ensemble."""
+        if (value != value.lower() or value != value.title()):
+            sparql_query += "{"
+        sparql_query += f"?ark skos:prefLabel \"{value}\"@fr."
+        if (value != value.lower() or value != value.title()):
+            sparql_query += "}"
         if value != value.lower():
             sparql_query += """
   UNION
-  {?ark skos:prefLabel \"""" + value.lower() + """\".}"""
+  {?ark skos:prefLabel \"""" + value.lower() + """\"@fr.}"""
         if value.title() not in [value, value.lower()]:
             sparql_query += """
   UNION
-  {?ark skos:prefLabel \"""" + value.title() + """\".}"""
+  {?ark skos:prefLabel \"""" + value.title() + """\"@fr.}"""
         sparql_query += "\n?ark skos:prefLabel ?label.}"
         print(sparql_query)
+        endpoint = "https://data.bnf.fr/sparql"
         result_sparql = sparql2dict(endpoint, sparql_query, ["label", "ark"])
+        print("result_sparql", result_sparql)
         if result_sparql:
             subdivs[value] = result_sparql
         else:
             subdivs[value] = "non autonome"
+            print(value, "n'a pas sa propre notice")
     return subdivs
 
-if __name__ == __main__:
+def analyse_value(field, field_value, value, ark, xml_record, subdivs, report):
+    if value in subdivs:
+        if subdivs[value] == "non autonome":
+            line2report([ark, ark2nn(ark), field, field_value, value], report)
+    else:
+        query = f"aut.accesspoint adj \"{value}\" and aut.type any RAM"
+        params = {"recordSchema": "intermarcxchange", "maximumRecords": "500"}
+        results = sru.SRU_result(query, parametres=params)
+        print(results.url, results.nb_results)
+        for ark in results.dict_records:
+            record = results.dict_records[ark]
+            accesspoint = sru.record2fieldvalue(record, "166")
+            f166a = sru.record2fieldvalue(record, "166$a")
+            if accesspoint:
+                subfields = sru.field2listsubfields(record.xpath("*[@tag='166']")[0])
+                if subfields == "w a" or subfields == "a":
+                    if udecode(f166a).lower() == udecode(value.lower()):
+                        subdivs[value] = [ark]
+        i = 501
+        while i < results.nb_results:
+            params["startRecord"] = str(i)
+            next_results = sru.SRU_result(query, parametres=params)
+            for ark in next_results.dict_records:
+                record = next_results.dict_records[ark]
+                accesspoint = sru.record2fieldvalue(record, "166")
+                f166a = sru.record2fieldvalue(record, "166$a")
+                if accesspoint:
+                    subfields = sru.field2listsubfields(record.xpath("*[@tag='166']")[0])
+                    if subfields == "w a" or subfields == "a":
+                        if udecode(f166a).lower() == udecode(value.lower()):
+                            subdivs[value] = [ark]
+            i += 500
+    if value not in subdivs:
+        subdivs[value] = "non autonome"
+        line2report([ark, ark2nn(ark), field, field_value, value], report)
+    return subdivs
+
+
+if __name__ == "__main__":
     report = create_file("Subdiv Rameau non autonomes.txt", "ARK,NNA,Zone,Label complet,Subdiv".split(","))
     extract_rameau(report)
