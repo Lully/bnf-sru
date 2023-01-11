909a865cfecb84cbe9a71c9beaa7cd34f962b944
Lully <lully1804@yahoo.fr>
2023-01-11 18:52:54 +0100

    Update splitmp4.py

--- splitmp4.py
+++ splitmp4.py
@@ -1,35 +1,38 @@
 # coding: utf-8
 
+"""
+Découpage d'un fichier
+"""
+
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from moviepy.editor import *
 import os
 
-dirname = r"D:\Documents\perso\Géraud\24\espagnol"
-filename = "video.mp4"
+dirname = r""
+filename = r""
 
 # Replace the filename below.
 required_video_file = os.path.join(dirname, filename)
 
-times = [[0, 300], 
-         [300, 600],
-         [600, 900]
+times = [[15, 28], 
+         [33, 66],
+         [70, 97]
         ]
 
-for time in times:
-  starttime = int(time.split("-")[0])
-  endtime = int(time.split("-")[1])
-  ffmpeg_extract_subclip(required_video_file, starttime, endtime, targetname=os.path.join(dirname, str(times.index(time)+1)+".mp4"))
-
 def combine_videos_folder_to_one(folder_path):
-    L =[]
-    for root, dirs, files in os.walk(folder_path):
-        #files.sort()
-        for file in files:
-            if file != filename and file.endswith("mp4"):
-                filePath = os.path.join(root, file)
-                video = VideoFileClip(filePath)
-                L.append(video)
-    final_clip = concatenate_videoclips(L,method='compose')
-    final_clip.to_videofile("compilation_output.mp4", fps=60, remove_temp=False)
+    filename = os.path.join(dirname, "video.mp4")
+    clip = VideoFileClip(filename)
+    L = []
+    for time in times:
+      subclip = clip.subclip(time[0], time[1]+1)
+      L.append(subclip)
+    print("\nconcaténation de toutes les videos")
+    final_clip = concatenate_videoclips(L, method='chain')
+    final_clip.write_videofile(os.path.join(dirname, "compilation_output.mp4"))
+    try:
+      os.remove("compilation_outputTEMP_MPY_wvf_snd.mp3")
+    except FileNotFoundError:
+      pass
 
     
+combine_videos_folder_to_one(dirname)
